eeab5f6e1a4738dc5c0828ae197446dea29cce6c
hieu15402 <hieunm15402@gmail.com>
2024-10-10 19:50:19 +0700

    Fix conflict

--- TP4SCS.Solution/TP4SCS.API/Program.cs
+++ TP4SCS.Solution/TP4SCS.API/Program.cs
@@ -8,7 +8,6 @@ using TP4SCS.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
