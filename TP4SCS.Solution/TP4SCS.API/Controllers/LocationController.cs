5655df280dae55160011cf91f2ac64eef922aea4
Lê Ngô Hiệp Quốc <117381050+hiepquoc0607@users.noreply.github.com>
2024-11-02 01:06:50 +0700

    Implement Feature For Location Table

---
+++ TP4SCS.Solution/TP4SCS.API/Controllers/LocationController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP4SCS.Services.Interfaces;
+
+namespace TP4SCS.API.Controllers
+{
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationService _locationService;
+
+        public LocationController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+
+        [HttpGet]
+        [Route("api/locations/cities")]
+        public async Task<IActionResult> GetCitiesAsync()
+        {
+            var result = await _locationService.GetCityAsync();
+
+            if (result.StatusCode != 200)
+            {
+                return StatusCode(result.StatusCode, result);
+            }
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("api/locations/{cityName}/wards")]
+        public async Task<IActionResult> GetWardsByCityAsync([FromRoute] string cityName)
+        {
+            var result = await _locationService.GetWardByCityAsync(cityName);
+
+            if (result.StatusCode != 200)
+            {
+                return StatusCode(result.StatusCode, result);
+            }
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("api/locations/{wardName}/provinces")]
+        public async Task<IActionResult> GetProvincesByWardAsync([FromRoute] string wardName)
+        {
+            var result = await _locationService.GetProviceByWardAsync(wardName);
+
+            if (result.StatusCode != 200)
+            {
+                return StatusCode(result.StatusCode, result);
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ TP4SCS.Solution/TP4SCS.Repositry/Interfaces/ILocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using TP4SCS.Library.Models.Data;
+
+namespace TP4SCS.Repository.Interfaces
+{
+    public interface ILocationRepository : IGenericRepository<Location>
+    {
+        Task<IEnumerable<Location>?> GetCityAsync();
+
+        Task<IEnumerable<Location>?> GetWardByCityAsync(string name);
+
+        Task<IEnumerable<Location>?> GetProvinceByWardAsync(string name);
+    }
+}
