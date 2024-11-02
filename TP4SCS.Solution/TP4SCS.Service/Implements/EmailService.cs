81ca0313944ef45e114bb6b44d696e233c740c68
hieu15402 <hieunm15402@gmail.com>
2024-11-02 15:06:27 +0700

    Update send mail

---
+++ TP4SCS.Solution/TP4SCS.Service/Implements/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Options;
+using TP4SCS.Library.Utils.Email;
+using TP4SCS.Services.Interfaces;
+
+public class EmailService : IEmailService
+{
+    private readonly EmailSettings _emailSettings;
+
+    public EmailService(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string body)
+    {
+        var smtpClient = new SmtpClient(_emailSettings.SmtpServer)
+        {
+            Port = _emailSettings.SmtpPort,
+            Credentials = new NetworkCredential(_emailSettings.SenderEmail, _emailSettings.SenderPassword),
+            EnableSsl = true,
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(_emailSettings.SenderEmail),
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true,
+        };
+
+        mailMessage.To.Add(toEmail);
+
+        await smtpClient.SendMailAsync(mailMessage);
+    }
+}
