a95157769cf1fe4fcb18a7ff58fdb3fa4851c7a8
Lê Ngô Hiệp Quốc <117381050+hiepquoc0607@users.noreply.github.com>
2024-11-21 06:57:48 +0700

    Update Business API

--- TP4SCS.Solution/TP4SCS.Library/Models/Request/Business/UpdateBusinessRequest.cs
+++ TP4SCS.Solution/TP4SCS.Library/Models/Request/Business/UpdateBusinessRequest.cs
@@ -12,6 +12,15 @@ namespace TP4SCS.Library.Models.Request.Business
         [DefaultValue("string")]
         public string Phone { get; set; } = string.Empty;
 
+        [Required]
+        [MinLength(12), MaxLength(12)]
+        [DefaultValue("string")]
+        public string CitizenId { get; set; } = string.Empty;
+
+        public string FrontCitizenImageUrl { get; set; } = string.Empty;
+
+        public string BackCitizenImageUrl { get; set; } = string.Empty;
+
         public string ImageUrl { get; set; } = string.Empty;
 
         public string Status { get; set; } = string.Empty;
