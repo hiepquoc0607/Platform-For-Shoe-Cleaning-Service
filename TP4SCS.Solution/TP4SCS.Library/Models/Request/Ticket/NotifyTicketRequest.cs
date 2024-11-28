0481180c418d36ff5dc932988a15756e23d0ea16
Lê Ngô Hiệp Quốc <117381050+hiepquoc0607@users.noreply.github.com>
2024-11-29 01:20:59 +0700

    Update Ticket API

---
+++ TP4SCS.Solution/TP4SCS.Library/Models/Request/Ticket/NotifyTicketRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP4SCS.Library.Models.Request.Ticket
+{
+    public class NotifyTicketRequest
+    {
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        public int TicketId { get; set; }
+    }
+}
