969fcc1bd46f24f8cae74209ab3921cea0ade72c
hieu15402 <hieunm15402@gmail.com>
2024-12-03 19:20:47 +0700

    Update Feedback Api

--- TP4SCS.Solution/TP4SCS.Library/Models/Response/Feedback/FeedbackResponseForAdmin.cs
+++ TP4SCS.Solution/TP4SCS.Library/Models/Response/Feedback/FeedbackResponseForAdmin.cs
@@ -16,7 +16,7 @@ namespace TP4SCS.Library.Models.Response.Feedback
         public bool IsValidContent { get; set; }
 
         public bool IsValidAsset { get; set; }
-
+        public bool IsAllowedUpdate { get; set; }
         public string Status { get; set; } = null!;
 
         public virtual ICollection<AssetUrlResponse> AssetUrls { get; set; } = new List<AssetUrlResponse>();
