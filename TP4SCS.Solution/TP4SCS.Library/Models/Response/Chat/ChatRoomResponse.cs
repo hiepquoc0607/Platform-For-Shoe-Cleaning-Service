02724f977cece9ffcbca098d2037525097cc589d
Lê Ngô Hiệp Quốc <117381050+hiepquoc0607@users.noreply.github.com>
2024-12-11 02:56:26 +0700

    Update Chat API

--- TP4SCS.Solution/TP4SCS.Library/Models/Response/Chat/ChatRoomResponse.cs
+++ TP4SCS.Solution/TP4SCS.Library/Models/Response/Chat/ChatRoomResponse.cs
@@ -8,11 +8,15 @@
 
         public string Account1FullName { get; set; } = string.Empty;
 
+        public string Account1ImageUrl { get; set; } = string.Empty;
+
         public bool IsAccount1Seen { get; set; }
 
         public int AccountId2 { get; set; }
 
         public string Account2FullName { get; set; } = string.Empty;
+
+        public string Account2ImageUrl { get; set; } = string.Empty;
 
         public bool IsAccount2Seen { get; set; }
     }
